668d91614ed2210b5f2daac6a7c4c24d29da408e
ZarmaMehboob <48102510+ZarmaMehboob@users.noreply.github.com>
2019-05-11 22:13:57 +0500

    ForEach

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+class ForEach {
+ public static void main(String arg[]){
+ int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+ int sum = 0;
+ for (int x : nums) {
+ System.out.println("value is: " + x);
+ sum += X;
+ }
+ System.out.println("summation: " + sum);
+ }
+}
